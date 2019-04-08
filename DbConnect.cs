fd4c2435b19f2ed54c5bf6a237d7dbbdd3f63f95
Prince Waichal <princeironman@gmail.com>
2019-04-09 00:56:16 +0530

    Add Projects

---
+++ DbConnect.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public class Connect
+{
+    public const string ConnectString = "server=localhost;user id=root;persistsecurityinfo=True;database=OMSDb;allowbatch=True;checkparameters=True;connectionlifetime=300;Integrated Security=True;interactivesession=True";
+}
